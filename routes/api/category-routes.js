8517e218d5b67f250211175ea2d5930a22ec9f1f
Jorge Viera <Jorge5psgt@gmail.com>
2021-06-27 21:26:56 -0400

    updated api pro

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,8 +47,8 @@ const { Category, Product } = require('../../models');
     }
   });
 
-  // update a category by its `id` value
-  // router.put('/:id', async(req, res) => {
+  // // update a category by its `id` value
+   router.put('/:id', async(req, res) => {
   //   try {
   //     const categoryData = await Category.update(req.body, {
   //     where: {
@@ -57,21 +57,22 @@ const { Category, Product } = require('../../models');
   //   })
   //   .then((Category) => {
   //     // find all associated tags from ProductTag
-  //     return ProductTag.findAll({ where: { product_id: req.params.id } });
+  //     return Product.findAll({ where: { product_id: req.params.id } });
   //   })
   //   .then((productTags) => {
   //     // get list of current tag_ids
   //     const productTagIds = productTags.map(({ tag_id }) => tag_id);
   //     // create filtered list of new tag_ids
   //     const newProductTags = req.body.tagIds
-  //       .filter((tag_id) => !productTagIds.includes(tag_id))
+  //       .filter((tag_id) => {
+  //         (!productTagIds.includes(tag_id))
   //       .map((tag_id) => {
   //         return {
   //           product_id: req.params.id,
   //           tag_id,
-  //         };
-  //       });
-
+  //        };
+  });
+      
   // delete a category by its `id` value
   router.delete('/:id', async (req, res) => {
     try {
